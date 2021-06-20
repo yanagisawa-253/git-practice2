e9ac5648f776b7a402712965596470e66a4701de
yanagisawa-253 <anaguma253@gmail.com>
2021-06-20 13:32:06 +0000

    sample_app

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '95e2f2e998598adeb943fd19923406f1ba6095cc73d1aee951e8d2b5e70742979015b17664f965ed23fb622c2ef4da686596b069572675da8c725f94996e0382'
