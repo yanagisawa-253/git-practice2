b629fbfee1580f44cce27cf8fbc95d269807b7e7
yanagisawa-253 <anaguma253@gmail.com>
2021-06-23 12:30:17 +0000

    [update]8章まで

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '3e6b4bc878a63b0fcbad47da8e282c5ce2d24d8684e2b702d0a1448aca357859366bb5382e308daf1cd1e631b5bab527bf59fb896f4249f75f4f61832c73d9a4'
+
