57e3c6d0479cbe76a31c83d9cbc8eaa57d173470
yanagisawa-253 <anaguma253@gmial.com>
2021-09-10 11:25:54 +0000

    [add]cart_itemcontroller記述

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,27 @@
 class Public::CartItemsController < ApplicationController
+    def index
+    @cart_items = CartItem.all
+  end
+  
+  def create
+    @cart_item.save
+    if 
+      
+    else
+      
+    end
+  end
+  
+  def destroy
+    
+  end
+  
+  def destroy_all
+    current_user.books.destroy_all
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,12 @@ class Item < ApplicationRecord
   has_many :order_items
   
   attachment :image
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
+  def subtotal
+    item.with_tax_price * amount
+  end
 end
